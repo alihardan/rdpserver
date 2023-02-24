d72be60088dbabdc2745051616702cd188da1151
alihardan <alihardanc@gmail.com>
2023-02-25 00:55:32 +0330

    first commit

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+net user administrator "%ADMINISTRATOR_PASSWORD%" /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo "use this command to make rdp server at your client:"
+echo "cloudflared access rdp --hostname https://blah.com --url rdp://localhost:3389"
